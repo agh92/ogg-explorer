c16b0bc6b05d6c2c49583dcc91558e94ed5ef26e
Andres G <agh92@hotmail.com>
2021-05-21 12:31:00 +0200

    first steps towards building a distributable package with electron packager

--- projects/main/app.ts
+++ projects/main/app.ts
@@ -7,7 +7,9 @@ console.log(process.cwd());
 
 function initApp() {
   const appInitilizerService = new AppInitilizerService();
-  appInitilizerService.initApp(path.join(process.cwd(), 'dist', 'index.html'));
+  // appInitilizerService.initApp(path.join(process.cwd(), 'dist', 'index.html'));
+  // TODO check this + copy node moudles to dist + package.json in dist has to have the modules imported in the main
+  appInitilizerService.initApp('/Users/andresgilherrera/Dev/memorial/dist/memorial-darwin-x64/memorial.app/Contents/Resources/app/index.html');
 }
 
 app.on('ready', initApp);
